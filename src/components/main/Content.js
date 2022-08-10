cb63bffb8dbc1521465f6104acb72200b60c9716
Heman Arun Daney <heman.daney@manpowergroup.com.sg>
2022-08-10 18:11:33 +0800

    carousel fixes

--- src/components/main/Content.js
+++ src/components/main/Content.js
@@ -2,9 +2,9 @@ import { Typography, Box, Grid, Stack,Button } from '@mui/material'
 import MultiCarousel from './MultiCarousel'
 const Content = (props)=>{
     return(
-        <Stack> 
-            <MultiCarousel items={props.newReleases}></MultiCarousel>
-            <MultiCarousel items={props.featuredPlaylists}></MultiCarousel>
+        <Stack sx={{mb:'100px'}}> 
+            <MultiCarousel items={props.newReleases} title='RELEASED THIS WEEK'></MultiCarousel>
+            <MultiCarousel items={props.featuredPlaylists} title='FEATURED PLAYLIST'></MultiCarousel>
         </Stack> 
     )
 }
--- src/components/main/MultiCarousel.js
+++ src/components/main/MultiCarousel.js
@@ -2,22 +2,52 @@ import { Typography, Box, Grid, Stack, Button } from '@mui/material'
 import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
 import useWindowDimensions from '../../utils/Hooks/useWindowsDimensions';
 import 'pure-react-carousel/dist/react-carousel.es.css';
+import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
+import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
 
 const MultiCarousel = (props) => {
     const { width } = useWindowDimensions();
     return (
         <CarouselProvider
-            visibleSlides={width > 1300 ? 5: width > 1030 ? 4 : width > 870 ? 3: width > 630 ? 2 : 1 }
+            visibleSlides={width > 1450 ? 4 : width > 1132 ? 3 : width > 870 ? 2 : 1}
             step={5}
             naturalSlideWidth={150}
-            naturalSlideHeight={100}
+            naturalSlideHeight={width > 1450 ? 150 : width > 1132 ? 220 : width > 870 ? 200 : 150 }
             infinite
             totalSlides={props.items.length}
+           
         >
-            <ButtonBack>Back</ButtonBack>
-            <ButtonNext>Next</ButtonNext>
+            <Box sx={{display:'flex', width:'90%', margin:'0 auto',justifyContent:'space-between',  mb:'20px' }}>
+                <Typography sx={{color:'primary.typo', fontSize:'17px', fontWeight:'bold'}}> 
+                    {props.title}
+                </Typography>
+                <Box>
+                    <ButtonBack style={{
+                        background: 'none',
+                        color: 'inherit',
+                        border: 'none',
+                        padding: 0,
+                        outline: 'inherit',
+                    }}><KeyboardArrowLeftIcon /></ButtonBack>
+                    <ButtonNext style={{
+                        background: 'none',
+                        color: 'inherit',
+                        border: 'none',
+                        padding: 0,
+                        outline: 'inherit',
+                    }}><KeyboardArrowRightIcon /></ButtonNext>
+                </Box>
+                
+            </Box>
             <Slider>
-                {props.items.map((item, index) => <Slide index={index}><img style={{ height: '200px', width: '200px' }} src={item.images[0].url}></img></Slide>)}
+                {props.items.map((item, index) => <Slide index={index}>
+
+                    <img style={{ height: '300px', width: '300px' }} src={item.images[0].url}></img>
+                    <Typography>
+                        {item.name}
+                    </Typography>
+
+                </Slide>)}
             </Slider>
         </CarouselProvider>
     )
